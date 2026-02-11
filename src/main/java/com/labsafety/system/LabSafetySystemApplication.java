67ed80e2748571573703763007a1d22e98526463
ChenghanLiu <cliu@uottawa.ca>
2026-02-11 16:49:53 +0800

    Bootstrap lab safety system

---
+++ src/main/java/com/labsafety/system/LabSafetySystemApplication.java
@@ -0,0 +1,13 @@
+package com.labsafety.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSafetySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSafetySystemApplication.class, args);
+    }
+
+}
